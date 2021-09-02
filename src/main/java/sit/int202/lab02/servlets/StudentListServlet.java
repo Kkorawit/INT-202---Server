2166d6286e82655c4871198975e37092cd58b5dd
Kkorawit <korawit.sit@mail.kmutt.ac.th>
2021-09-02 22:53:12 +0700

    Student List-html

--- src/main/java/sit/int202/lab02/servlets/StudentListServlet.java
+++ src/main/java/sit/int202/lab02/servlets/StudentListServlet.java
@@ -17,22 +17,24 @@ public class StudentListServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         StudentRepository studentRepository = new StudentRepository();
-        try (PrintWriter out = response.getWriter()) {
-            out.println("<html>");
-            out.println("<body style='padding: 100px'>");
-            out.println("<h3>Student List ::</h3><hr>");
-            out.println("<table>");
-            for (Student student : studentRepository.all()) {
-                out.println("<tr><td>Id: </td> <td>" + student.getId() + "</td></tr>");
-                out.println("<tr><td>Name: </td> <td>" + student.getName() + "</td></tr>");
-                out.println("<tr><td>GPAX: </td> <td>" + student.getGpax() + "</td></tr>");
-                out.println("<tr><td colspan='2'><hr></td></tr>");
-            }
-            out.println("</table>");
-            out.println("<br>");
-            out.println("<a href='index.jsp'> [Go Back] </a>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+//        try (PrintWriter out = response.getWriter()) {
+//            out.println("<html>");
+//            out.println("<body style='padding: 100px'>");
+//            out.println("<h3>Student List ::</h3><hr>");
+//            out.println("<table>");
+//            for (Student student : studentRepository.all()) {
+//                out.println("<tr><td>Id: </td> <td>" + student.getId() + "</td></tr>");
+//                out.println("<tr><td>Name: </td> <td>" + student.getName() + "</td></tr>");
+//                out.println("<tr><td>GPAX: </td> <td>" + student.getGpax() + "</td></tr>");
+//                out.println("<tr><td colspan='2'><hr></td></tr>");
+//            }
+//            out.println("</table>");
+//            out.println("<br>");
+//            out.println("<a href='index.jsp'> [Go Back] </a>");
+//            out.println("</body>");
+//            out.println("</html>");
+//        }
+        request.setAttribute("students", studentRepository.all());
+        getServletContext().getRequestDispatcher("/StudentList.jsp").forward(request,response);
     }
 }
